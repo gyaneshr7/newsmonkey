0a291da5b3ecfc89608e1b83f05e8af03191ae6c
GYANESH <gyaneshr7@gmail.com>
2023-10-10 10:26:24 +0530

    Initial commit

---
+++ src/components/News.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NewsItem from './NewsItem'
+
+export default function News() {
+  return (
+    <div>
+        This is a news component
+        <NewsItem />
+        <NewsItem />
+        <NewsItem />
+        <NewsItem />
+        <NewsItem />
+    </div>
+  )
+}
